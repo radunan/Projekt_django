9fca64897e2b391ea5deb6370762c1d51a7c4070
radunan <novakkradekk@gmail.com>
2023-04-12 22:44:49 +0200

    Hotovo

--- games/views.py
+++ games/views.py
@@ -16,7 +16,7 @@ User = get_user_model()"""
 @login_required
 def index(request):
     games = Game.objects.all().order_by('-time_start')
-    gamePlayers = PlayerGame.objects.all()
+    gamePlayers = PlayerGame.objects.all()                          #výpis hráčů na indexu
     is_admin_or_player = is_player_or_admin(request.user)
     context = {'games': games, 'gameplayers': gamePlayers, 'is_admin_or_player': is_admin_or_player}
     return render(request, 'index.html', context)
@@ -25,7 +25,7 @@ def index(request):
 @login_required
 def detail(request, id):
     game = Game.objects.get(pk=id)
-    players = PlayerGame.objects.filter(game=id)
+    players = PlayerGame.objects.filter(game=id)                #detail hry
     is_admin_or_player = is_player_or_admin(request.user)
 
     if request.method == 'POST' and game.active:
@@ -33,7 +33,7 @@ def detail(request, id):
         game.active = False
 
         winner = game.get_winner()
-        winner_name = winner.username if winner else None
+        winner_name = winner.username if winner else None         #získávání výherce hry
         game.winner = winner_name
         game.save()
 
@@ -45,27 +45,25 @@ def detail(request, id):
 @login_required
 def delete(request, id):
     game = Game.objects.get(pk=id)
-    game.delete()
+    game.delete()                           #mazání her
     return HttpResponseRedirect('/')
 
 
 def deletePlayer(request, id):
     gamePlayer = PlayerGame.objects.get(pk=id)
-    game_id = gamePlayer.game.id
+    game_id = gamePlayer.game.id                    #mazání hráčů ve hře
     is_current_winner = False
 
-    # Check if the deleted player is the current winner
     game = Game.objects.get(id=game_id)
     if game.winner == gamePlayer.player.username:
         is_current_winner = True
 
     gamePlayer.delete()
 
-    # If the deleted player was the current winner, update the winner to the player with the highest score
     if is_current_winner:
         winner = game.get_winner()
         if winner:
-            game.winner = winner.playergame_set.first().player.username
+            game.winner = winner.playergame_set.first().player.username         #pokud je mazaný hráč výherce musí se přepnout vítěz
         else:
             game.winner = None
         game.save()
@@ -93,7 +91,7 @@ def gameEdit(request, id):
 
 
 def is_player_or_admin(user):
-    if user.is_superuser:
+    if user.is_superuser:                   #funkce na určení zda je užívatel admin nebo hráč
         return user.is_superuser
     else:
         return user.groups.filter(name='Hráč').exists()
@@ -101,18 +99,18 @@ def is_player_or_admin(user):
 
 @login_required
 @user_passes_test(is_player_or_admin, login_url='/')
-def add(request):
+def add(request):                                                   #přidávání her
     active_games = Game.objects.filter(active=True).count()
 
     if active_games >= 2 and request.POST.get('active'):
-        error_message = "Nelze mít více než 2 aktivní hry. Vydrž než hry skončí :)"
+        error_message = "Nelze mít více než 2 aktivní hry. Vydrž než hry skončí :)"       #počet aktivních her nesmí být větší než 2
         return render(request, 'gameAdd.html', {'form': GameForm(), 'error_message': error_message})
 
     if request.method == 'POST':
         form = GameForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data['title']
-            time_start = timezone.now() if form.cleaned_data['active'] else form.cleaned_data['time_start']
+            time_start = timezone.now() if form.cleaned_data['active'] else form.cleaned_data['time_start']    #pokud je hra aktivní nastaví se aktuální čas
             time_end = form.cleaned_data['time_end']
             description = form.cleaned_data['description']
             author = request.user
@@ -128,7 +126,7 @@ def add(request):
 
 @login_required
 @user_passes_test(is_player_or_admin, login_url='/')
-def addGamePlayers(request):
+def addGamePlayers(request):                #přidávání hráčů do hry
     if request.method == 'POST':
         form = GamePlayersForm(request.POST)
         if form.is_valid():
@@ -138,7 +136,7 @@ def addGamePlayers(request):
             get_player = User.objects.get(username=player[0])
             get_game = Game.objects.get(title=game[0])
 
-            if PlayerGame.objects.filter(game=get_game, player=get_player).exists():
+            if PlayerGame.objects.filter(game=get_game, player=get_player).exists():      #pokud hráč se ve hře nachází, už nelze přidat
                 error_message = f"Hráč {get_player.username} už byl přidán do hry."
                 form.add_error('player', error_message)
                 return render(request, 'gamePlayersAdd.html', {'form': form})
@@ -147,7 +145,7 @@ def addGamePlayers(request):
 
             if not get_game.active:
                 winner = get_game.get_winner()
-                get_game.winner = winner.playergame_set.first().player.username
+                get_game.winner = winner.playergame_set.first().player.username         #nastavování výherce pokud hra neni aktivní
                 get_game.save()
 
             return HttpResponseRedirect(reverse('detail', args=[get_game.pk]))
@@ -160,5 +158,5 @@ def addGamePlayers(request):
 @login_required
 @user_passes_test(is_player_or_admin, login_url='/')
 def userGames(request):
-    userGames = PlayerGame.objects.filter(player=request.user)
+    userGames = PlayerGame.objects.filter(player=request.user).order_by('-game__time_start')                  #historie hráče
     return render(request, 'user_games.html', {'userGames': userGames})
